a01a61e6fb90fc806f05e99c5f87e4f8aec6153f
stpholise <stpholise@gmail.com>
2024-10-16 12:58:35 +0100

    Edit graph

--- src/Components/Graph.jsx
+++ src/Components/Graph.jsx
@@ -4,17 +4,25 @@ import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement
 
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, Title, Tooltip, Legend);
 
-const Graph = () => {
+
+
+const Graph = (
+
+  // dataSetValue = [
+    
+  // ]
+
+) => {
 
     const LineChartData = {
         labels: [
-            1,2,3,4,5,6,7,8,9,10
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
             
         ],
         datasets: [ 
             {
                 label: 'First',
-                data: [0, 600, 100, 100, 3000, 400, 1004, 500,0],
+                data: [0,55000, 100600,110000, 35000, 25000, 83000, 155400, 175800, 175000, 130000, 105000, 80000, 50000, 20000, 1000, 0],
                 borderColor: 'red',
                 backgroundColor: 'red',
                 fill: false,
